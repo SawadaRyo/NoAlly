dd89c04b86e31b46f3e9d567cdc6bd70fa73cbb1
SR21C <rysawad@gmail.com>
2022-03-19 18:55:07 +0900

    GameUI Update

--- unityProject2DActionGameA/Assets/Scripts/PlayerContoller.cs
+++ unityProject2DActionGameA/Assets/Scripts/PlayerContoller.cs
@@ -8,19 +8,22 @@ public class PlayerContoller : MonoBehaviour
     [SerializeField, Tooltip("ダッシュの倍率")] float dashPowor = 10f;
     [SerializeField, Tooltip("空中での移動速度")] float grindFroth = 5f;
     [SerializeField, Tooltip("プレイヤーのジャンプ力")] float jump = 3f;
-    [SerializeField, Tooltip("Rayの射程")] float distance = 0.1f;//
+    [SerializeField, Tooltip("接地判定のRayの射程")] float distance = 0.1f;
+    [SerializeField, Tooltip("壁の接触判定のRayの射程")] float walldistance = 0.1f;
     [SerializeField, Tooltip("SphierCastの半径")] float isGroundRengeRadios = 1f;
     [SerializeField, Tooltip("壁ジャンの力")] float wallJumpPower = 10f;
     [Tooltip("プレイヤーに掛かる重力値")] float m_gravity = -0.981f;
     [SerializeField, Tooltip("ジャンプのサウンド")] AudioClip jumpSound;
     [SerializeField, Tooltip("プレイヤーのダメージサウンド")] AudioClip damageSound;
+
     [SerializeField, Tooltip("Rayの射出点")] Transform footPos;
     [SerializeField, Tooltip("接地判定のLayerMask")] LayerMask groundMask = ~0;
     [SerializeField, Tooltip("壁の接触判定")] LayerMask wallMask = ~0;
+
+    [SerializeField, Tooltip("アニメーションを取得する為の変数")] Animator m_Animator;
     [SerializeField, Tooltip("GamManagerを格納する変数")] GameManager m_gameManager;
-    [SerializeField, Tooltip("メイン武器")] GameObject m_mainWeapon = default;
-    [SerializeField, Tooltip("サブ武器")] GameObject m_subWeapon = default;
-    [SerializeField, Tooltip("武器prefabを格納する変数")] GameObject[] m_weaponPrefab = new GameObject[3];
+
+    [SerializeField, Tooltip("武器prefabを格納する変数")] GameObject[] m_weaponPrefab = new GameObject[4];
 
     enum UnityChanState { STAND, WALK, DASH, JUMP };
     float m_turnSpeed = 15f;
@@ -30,8 +33,9 @@ public class PlayerContoller : MonoBehaviour
     //List<ItemBase> m_ItemList = new List<ItemBase>();
     //Camera m_MainCamera;
     [Tooltip("オーディオを取得する為の変数")] AudioSource m_Audio;
-    [SerializeField, Tooltip("アニメーションを取得する為の変数")] Animator m_Animator;
-    [Tooltip("Unityちゃんの向いている方向")] Quaternion orgLocalQuaternion;
+    [Tooltip("Playerの向いている方向")] Quaternion orgLocalQuaternion;
+    [Tooltip("メイン武器")] GameObject m_mainWeapon = default;
+    [Tooltip("サブ武器")] GameObject m_subWeapon = default;
     Collider isAttack;
     GameObject m_equipmentWeapon = default;
     RaycastHit m_hitInfo;
@@ -42,7 +46,13 @@ public class PlayerContoller : MonoBehaviour
         orgLocalQuaternion = this.transform.localRotation;
         m_rb = GetComponent<Rigidbody>();
         m_Audio = gameObject.AddComponent<AudioSource>();
+
         m_weaponSwitch = true;
+        m_mainWeapon = m_weaponPrefab[0];
+        m_subWeapon = m_weaponPrefab[1];
+        m_mainWeapon.SetActive(true);
+        m_subWeapon.SetActive(false);
+        m_equipmentWeapon = m_mainWeapon;
         isAttack = m_equipmentWeapon.GetComponent<Collider>();
         //m_gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
     }
@@ -54,6 +64,7 @@ public class PlayerContoller : MonoBehaviour
             JumpMethod();
             wallJumpMethod();
             MoveMethod();
+            WeaponActionMethod();
         }
     }
 
@@ -75,7 +86,7 @@ public class PlayerContoller : MonoBehaviour
         //プレイヤーの移動
         if (m_h != 0)
         {
-            if (Input.GetButton("Dash"))//ダッシュコマンド
+            if (Input.GetButton("Dash") && IsGrounded())//ダッシュコマンド
             {
                 velocity.x = m_h * dashPowor;
             }
@@ -86,12 +97,6 @@ public class PlayerContoller : MonoBehaviour
         }
         m_rb.velocity = new Vector3(velocity.x, m_rb.velocity.y, 0);
         m_Animator.SetFloat("MoveSpeed", Mathf.Abs(velocity.x));
-
-        //プレイヤーの攻撃
-        if (Input.GetButtonDown("Attack"))
-        {
-
-        }
     }
 
     void OnCollisionEnter(Collision other)
@@ -120,7 +125,7 @@ public class PlayerContoller : MonoBehaviour
     void OnCollisionExit()
     {
         transform.parent = null;
-        this.transform.localRotation = orgLocalQuaternion;
+        //this.transform.localRotation = orgLocalQuaternion;
     }
 
     bool IsGrounded()
@@ -150,7 +155,7 @@ public class PlayerContoller : MonoBehaviour
     {
         Vector3 isWallCenter = footPos.transform.position;
         Ray ray = new Ray(isWallCenter, Vector3.right);
-        bool hitFlg = Physics.Raycast(ray, out m_hitInfo, distance, wallMask);
+        bool hitFlg = Physics.Raycast(ray, out m_hitInfo, walldistance, wallMask);
         return hitFlg;
     }
 
@@ -158,7 +163,7 @@ public class PlayerContoller : MonoBehaviour
     {
         Vector3 isWallCenter = footPos.transform.position;
         Ray ray = new Ray(isWallCenter, Vector3.left);
-        bool hitFlg = Physics.Raycast(ray, out m_hitInfo, distance, wallMask);
+        bool hitFlg = Physics.Raycast(ray, out m_hitInfo, walldistance, wallMask);
         return hitFlg;
     }
 
@@ -232,29 +237,28 @@ public class PlayerContoller : MonoBehaviour
 
     void WeaponActionMethod()
     {
-        
-
         //攻撃
         if(Input.GetButtonDown("Attack"))
         {
             m_Animator.SetTrigger("Attack");
         }
-        //メインとサブの武器を切り替える
-        if(m_weaponSwitch)
-        {
-            m_equipmentWeapon = m_mainWeapon;
-        }
-        else
-        {
-            m_equipmentWeapon = m_subWeapon;
-        }
-
+        
         //メインとサブの表示を切り替える
         if (Input.GetButtonDown("WeaponChange"))
         {
             m_weaponSwitch = !m_weaponSwitch;
             m_mainWeapon.SetActive(m_weaponSwitch);
             m_subWeapon.SetActive(!m_weaponSwitch);
+        }
+
+        //メインとサブの武器を切り替える
+        if (m_weaponSwitch)
+        {
+            m_equipmentWeapon = m_mainWeapon;
+        }
+        else
+        {
+            m_equipmentWeapon = m_subWeapon;
         }
     }
 
