0635d214d33ae5ff77360e519c0ed4c0d428711e
SR21C <rysawad@gmail.com>
2022-04-25 15:44:01 +0900

    Uppdate

--- unityProject2DActionGameA/Assets/Scripts/PlayerContoller.cs
+++ unityProject2DActionGameA/Assets/Scripts/PlayerContoller.cs
@@ -6,46 +6,37 @@ using UnityEngine;
 public class PlayerContoller : MonoBehaviour
 {
     [SerializeField, Tooltip("溜め攻撃の溜めカウンター")] int m_chrageAttackCounter = 120;
-    [SerializeField, Tooltip("プレイヤーの移動速度")] float speed = 5f;
-    [SerializeField, Tooltip("ダッシュの倍率")] float dashPowor = 10f;
-    [SerializeField, Tooltip("空中での移動速度")] float grindFroth = 5f;
-    [SerializeField, Tooltip("プレイヤーのジャンプ力")] float jump = 3f;
-    [SerializeField, Tooltip("接地判定のRayの射程")] float distance = 0.1f;
-    [SerializeField, Tooltip("壁の接触判定のRayの射程")] float walldistance = 0.1f;
-    [SerializeField, Tooltip("SphierCastの半径")] float isGroundRengeRadios = 1f;
-    [SerializeField, Tooltip("壁ジャンの力")] float wallJumpPower = 10f;
-    [SerializeField, Tooltip("ジャンプのサウンド")] AudioClip jumpSound;
-    [SerializeField, Tooltip("プレイヤーのダメージサウンド")] AudioClip damageSound;
+    [SerializeField, Tooltip("プレイヤーの移動速度")] float m_speed = 5f;
+    [SerializeField, Tooltip("ダッシュの倍率")] float m_dashPowor = 10f;
+    [SerializeField, Tooltip("空中での移動速度")] float m_grindFroth = 5f;
+    [SerializeField, Tooltip("プレイヤーのジャンプ力")] float m_jump = 3f;
+    [SerializeField, Tooltip("接地判定のRayの射程")] float m_distance = 0.1f;
+    [SerializeField, Tooltip("壁の接触判定のRayの射程")] float m_walldistance = 0.1f;
+    [SerializeField, Tooltip("SphierCastの半径")] float m_isGroundRengeRadios = 1f;
+    [SerializeField, Tooltip("壁ジャンの力")] float m_wallJumpPower = 10f;
+    [SerializeField, Tooltip("ジャンプのサウンド")] AudioClip m_jumpSound;
+    [SerializeField, Tooltip("プレイヤーのダメージサウンド")] AudioClip m_damageSound;
 
-    [SerializeField, Tooltip("Rayの射出点")] Transform footPos;
-    [SerializeField, Tooltip("接地判定のLayerMask")] LayerMask groundMask = ~0;
-    [SerializeField, Tooltip("壁の接触判定")] LayerMask wallMask = ~0;
+    [SerializeField, Tooltip("Rayの射出点")] Transform m_footPos;
+    [SerializeField, Tooltip("接地判定のLayerMask")] LayerMask m_groundMask = ~0;
+    [SerializeField, Tooltip("壁の接触判定")] LayerMask m_wallMask = ~0;
 
     [SerializeField, Tooltip("アニメーションを取得する為の変数")] Animator m_Animator;
     [SerializeField, Tooltip("GamManagerを格納する変数")] GameManager m_gameManager;
+    [SerializeField, Tooltip("WeaponChangerを格納する変数")] WeaponChanger m_weaponChanger;
 
-    [SerializeField, Tooltip("武器prefabを格納する変数")] GameObject[] m_weaponPrefab = new GameObject[4];
-    [SerializeField, Tooltip("メイン武器")] GameObject m_mainWeapon = default;
-    [SerializeField, Tooltip("サブ武器")] GameObject m_subWeapon = default;
-
-
-    [Tooltip("溜め攻撃の溜め時間")] int m_chrageAttackCount = 0;
-    [Tooltip("Playerの振り向き速度")]float m_turnSpeed = 25f;
-    [Tooltip("横移動のベクトル")]float m_h;
-    [Tooltip("武器切り替え")] bool m_weaponSwitch = true;
+    
+    [Tooltip("移動力指定")] float m_moveSpeed = 0f;
+    [Tooltip("Playerの振り向き速度")] float m_turnSpeed = 25f;
+    [Tooltip("横移動のベクトル")] float m_h;
     [Tooltip("スライディングの判定")] bool m_isDash = false;
-    bool m_attacked = false;
     bool m_ableMove = false;
     [Tooltip("Rigidbodyコンポーネントの取得")]Rigidbody m_rb;
     //List<ItemBase> m_ItemList = new List<ItemBase>();
     [Tooltip("オーディオを取得する為の変数")] AudioSource m_Audio;
     //[Tooltip("Playerの向いている方向")] Quaternion orgLocalQuaternion;
-    [Tooltip("装備中の武器")] GameObject m_equipmentWeapon = default;
     RaycastHit m_hitInfo;
     public Vector3 NormalOfStickingWall { get; private set; } = Vector3.zero;
-    public bool Charge { get; private set; } = false;
-    public int ChrageAttackCount { get => m_chrageAttackCount; set => m_chrageAttackCount = value; }
-    public bool Attacked { get => m_attacked; set => m_attacked = value; }
     delegate void PlayerMethod();
     PlayerMethod m_playerMethod = default;
     
@@ -56,26 +47,16 @@ public class PlayerContoller : MonoBehaviour
         m_rb = GetComponent<Rigidbody>();
         m_Audio = gameObject.AddComponent<AudioSource>();
 
-        m_weaponSwitch = true;
-        //m_mainWeapon = m_weaponPrefab[0];
-        //m_subWeapon = m_weaponPrefab[1];
-        m_mainWeapon.SetActive(true);
-        m_subWeapon.SetActive(false);
-        m_equipmentWeapon = m_mainWeapon;
-        //m_gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
     }
     void OnEnable()
     {
         m_playerMethod += JumpMethod;
-        m_playerMethod += WeaponActionMethod;
         m_playerMethod += WallJumpMethod;
         m_playerMethod += MoveMethod;
-        Charge = Input.GetButton("Attack");
     }
     void OnDisable()
     {
         m_playerMethod -= JumpMethod;
-        m_playerMethod -= WeaponActionMethod;
         m_playerMethod -= WallJumpMethod;
         m_playerMethod -= MoveMethod;
     }
@@ -86,7 +67,6 @@ public class PlayerContoller : MonoBehaviour
         {
             m_playerMethod();
             //Debug.Log(m_chrageAttackCount);
-            Debug.Log(Attacked);
         }
     }
 
@@ -96,7 +76,6 @@ public class PlayerContoller : MonoBehaviour
     {
         m_h = Input.GetAxisRaw("Horizontal");
         m_isDash = Input.GetButtonDown("Dash");
-        var moveSpeed = 0f;
         Vector3 velocity = m_rb.velocity;
         if (m_h == -1)
         {
@@ -112,7 +91,7 @@ public class PlayerContoller : MonoBehaviour
         //プレイヤーの移動
         if (m_h != 0 && !m_ableMove)
         { 
-            moveSpeed = speed;
+            m_moveSpeed = m_speed;
         }
 
         //ダッシュコマンド
@@ -121,7 +100,8 @@ public class PlayerContoller : MonoBehaviour
             StartCoroutine("Dash", m_isDash);
             m_Animator.SetBool("Dash",m_isDash);
         }
-        velocity.x = m_h * moveSpeed;
+
+        velocity.x = m_h * m_moveSpeed;
         m_rb.velocity = new Vector3(velocity.x, m_rb.velocity.y, 0);
         m_Animator.SetFloat("MoveSpeed", Mathf.Abs(velocity.x));
     }
@@ -130,7 +110,7 @@ public class PlayerContoller : MonoBehaviour
     {
         if (other.gameObject.tag == "Damage")
         {
-            m_Audio.PlayOneShot(damageSound);
+            m_Audio.PlayOneShot(m_damageSound);
         }
 
         else if (other.gameObject.tag == "Pendulam" || other.gameObject.tag == "Ffield")
@@ -147,18 +127,18 @@ public class PlayerContoller : MonoBehaviour
 
     bool IsGrounded()
     {
-        Vector3 isGroundCenter = footPos.transform.position;
+        Vector3 isGroundCenter = m_footPos.transform.position;
         Ray ray = new Ray(isGroundCenter, Vector3.down);
-        bool hitFlg = Physics.SphereCast(ray, isGroundRengeRadios, out _, distance, groundMask);
+        bool hitFlg = Physics.SphereCast(ray, m_isGroundRengeRadios, out _, m_distance, m_groundMask);
         return hitFlg;
     }
     bool IsWalled()
     {
-        Vector3 isWallCenter = footPos.transform.position;
+        Vector3 isWallCenter = m_footPos.transform.position;
         Ray rayRight = new Ray(isWallCenter, Vector3.right);
         Ray rayLeft = new Ray(isWallCenter, Vector3.left);
-        bool hitFlg = Physics.Raycast(rayRight, out m_hitInfo, walldistance, wallMask)
-                   || Physics.Raycast(rayLeft, out m_hitInfo, walldistance, wallMask);
+        bool hitFlg = Physics.Raycast(rayRight, out m_hitInfo, m_walldistance, m_wallMask)
+                   || Physics.Raycast(rayLeft, out m_hitInfo, m_walldistance, m_wallMask);
         return hitFlg;
     }
     void JumpMethod()
@@ -167,8 +147,8 @@ public class PlayerContoller : MonoBehaviour
         if (Input.GetButtonDown("Jump") && IsGrounded())
         {
             //Debug.Log("a");
-            m_Audio.PlayOneShot(jumpSound);
-            m_rb.AddForce(0f, jump, 0f, ForceMode.Impulse);
+            m_Audio.PlayOneShot(m_jumpSound);
+            m_rb.AddForce(0f, m_jump, 0f, ForceMode.Impulse);
         }
         m_Animator.SetBool("Jump", !IsGrounded());
         
@@ -176,7 +156,7 @@ public class PlayerContoller : MonoBehaviour
     void WallJumpMethod()
     {
         m_Animator.SetBool("WallGrip",IsWalled());
-        m_equipmentWeapon.SetActive(!IsWalled());
+        m_weaponChanger.EquipmentWeapon.SetActive(!IsWalled());
         if (IsGrounded())
         {
             m_rb.useGravity = true;
@@ -204,7 +184,7 @@ public class PlayerContoller : MonoBehaviour
         {
             for (float i = 0; i <= 0.5f; i += 0.1f)
             {
-
+                m_moveSpeed = m_dashPowor;
             }
         }
         else if(!m_isDash)
@@ -216,13 +196,9 @@ public class PlayerContoller : MonoBehaviour
     //---------------------------------------------------------
     void WalljumpPower()
     {
-        m_Audio.PlayOneShot(jumpSound);
+        m_Audio.PlayOneShot(m_jumpSound);
         //Vector3 vec = transform.up + m_hitInfo.normal;
-        m_rb.AddForce(transform.up * wallJumpPower, ForceMode.Impulse);
-    }
-    void AttackJud()
-    {
-        Attacked = !Attacked;
+        m_rb.AddForce(transform.up * m_wallJumpPower, ForceMode.Impulse);
     }
     void MoveJud()
     {
@@ -230,45 +206,7 @@ public class PlayerContoller : MonoBehaviour
         Debug.Log(m_ableMove);
     }
 
-    void WeaponActionMethod()
-    {
-        //通常攻撃の処理
-        if(Input.GetButtonDown("Attack"))
-        {
-            m_Animator.SetTrigger(m_equipmentWeapon.name + "Attack");
-        }
-
-        //溜め攻撃の処理(弓矢のアニメーションもこの処理）
-        if (Charge && ChrageAttackCount < 1800 )
-        {
-            ChrageAttackCount++;
-        }
-        if (Input.GetButtonUp("Attack"))
-        {
-            if (ChrageAttackCount > 0)
-            {
-                ChrageAttackCount = 0;
-            }
-        }
-        m_Animator.SetBool("Charge", Input.GetButton("Attack"));
-        //メインとサブの表示を切り替える
-        if (Input.GetButtonDown("WeaponChange") && !Charge && !Attacked)
-        {
-            m_weaponSwitch = !m_weaponSwitch;
-            m_mainWeapon.SetActive(m_weaponSwitch);
-            m_subWeapon.SetActive(!m_weaponSwitch);
-
-            //メインとサブの武器を切り替える
-            if (m_weaponSwitch)
-            {
-                m_equipmentWeapon = m_mainWeapon;
-            }
-            else
-            {
-                m_equipmentWeapon = m_subWeapon;
-            }
-        }
-    }
+    
 
 #if UNITY_EDITOR
     //private void OnDrawGizmos()
