d3988016146056303d49eb1fa1cf3f023bb396fa
SR21C <rysawad@gmail.com>
2022-05-17 19:29:47 +0900

    Update

--- unityProject2DActionGameA/Assets/Scripts/WeaponsAction/Normal/BulletContoller.cs
+++ unityProject2DActionGameA/Assets/Scripts/WeaponsAction/Normal/BulletContoller.cs
@@ -7,12 +7,14 @@ public class BulletContoller : WeaponAction
 {
     [SerializeField] Transform m_muzzleForward = default;
     [SerializeField] GameObject[] m_bulletPrefab = new GameObject[3];
-    [SerializeField] float m_bulletSpeed = 10;
+    [SerializeField] PersonType m_personType = PersonType.Player;
     Bullet[] m_bullet = new Bullet[3];
     //ParticleSystem particle;
     int m_chargeLevel1 = 600;
     int m_chargeLevel2 = 1200;
     int m_bulletType = 0;
+
+    enum PersonType { Player,Enemy };
 
     public override void IsStart()
     {
@@ -25,26 +27,31 @@ public class BulletContoller : WeaponAction
     
     public override void WeaponChargeAttackMethod()
     {
-        //í èÌíe
-        if (m_chrageAttackCount <= m_chargeLevel1)
+        if (m_personType == PersonType.Player)
         {
-            m_bulletType = 0;
-        }
-        //ã≠óÕíe
-        else if (m_chrageAttackCount > m_chargeLevel1 && m_chrageAttackCount <= m_chargeLevel2)
-        {
-            m_bulletType = 1;
-        }
-        //ëÂñC
-        else if (m_chrageAttackCount > m_chargeLevel2)
-        {
-            m_bulletType = 2;
+            //í èÌíe
+            if (m_chrageAttackCount <= m_chargeLevel1)
+            {
+                m_bulletType = 0;
+            }
+            //ã≠óÕíe
+            else if (m_chrageAttackCount > m_chargeLevel1 && m_chrageAttackCount <= m_chargeLevel2)
+            {
+                m_bulletType = 1;
+            }
+            //ëÂñC
+            else if (m_chrageAttackCount > m_chargeLevel2)
+            {
+                m_bulletType = 2;
+            }
         }
     }
     void BulletInsAndResat()
     {
-        var bullletObj = Instantiate(m_bulletPrefab[m_bulletType], m_muzzleForward.position, Quaternion.Euler(0f,0f,90f)) as GameObject;
-        bullletObj.GetComponent<Rigidbody>().AddForce(m_muzzleForward.transform.forward * m_bulletSpeed, ForceMode.Impulse);
-        m_bulletType = 0; //ë≈ÇøèIÇÌÇ¡ÇΩå„íeÇÃprefabÇí èÌíeÇ…ñﬂÇ∑
+        if(m_personType == PersonType.Player)
+        {
+            var bullletObj = Instantiate(m_bulletPrefab[m_bulletType], m_muzzleForward.position, Quaternion.Euler(0f, 0f, 90f)) as GameObject;
+            m_bulletType = 0; //ë≈ÇøèIÇÌÇ¡ÇΩå„íeÇÃprefabÇí èÌíeÇ…ñﬂÇ∑
+        }
     }
 }
