31f2b3302b0b32efbee92cd819d498032df1eea5
SR21C <rysawad@gmail.com>
2022-09-21 18:06:26 +0900

    [Update] BrastarWeaponAnimation and Prefab

--- NoAlly/Assets/Scripts/Weapon/BrastarWeapon.cs
+++ NoAlly/Assets/Scripts/Weapon/BrastarWeapon.cs
@@ -5,45 +5,45 @@ using UnityEngine;
 
 public class BrastarWeapon : CombatWeapon
 {
-    bool m_isOpen = false;
-    float m_normalRigit = 0f;
-    float m_normalEleke = 0f;
-    float m_powerUpRigit = 3.5f;
-    float m_powerUpEleke = 5f;
-    Animator m_weaponAnimator = default;
+    Collider[] _hitJudgeCollideres;
+    bool _isOpen = false;
+    float _normalRigit = 0f;
+    float _normalEleke = 0f;
+    float _powerUpRigit = 3.5f;
+    float _powerUpEleke = 5f;
+    Animator _weaponAnimator = default;
+
+    public override void Awake()
+    {
+        base.Awake();
+
+        _weaponAnimator = GetComponent<Animator>();
+        _normalRigit = _rigitPower;
+        _normalEleke = _elekePower;
+    }
+    private void OnEnable()
+    {
+        //MainMenu.Instance.DisideElement += this.WeaponMode;
+    }
+    private void OnDisable()
+    {
+        //MainMenu.Instance.DisideElement -= this.WeaponMode;
+    }
     public override void WeaponMode(ElementType type)
     {
         base.WeaponMode(type);
         if(type == ElementType.ELEKE)
         {
-            _rigitPower = m_powerUpRigit;
-            _elekePower = m_powerUpEleke;
-            m_isOpen = true;
+            _rigitPower = _powerUpRigit;
+            _elekePower = _powerUpEleke;
+            _isOpen = true;
         }
         else
         {
-            _rigitPower = m_normalRigit;
-            _elekePower = m_normalEleke;
-            m_isOpen = false;
+            _rigitPower = _normalRigit;
+            _elekePower = _normalEleke;
+            _isOpen = false;
         }
-        m_weaponAnimator.SetBool("IsOpen",m_isOpen);
-    }
-    public override void Awake()
-    {
-        base.Awake();
-
-        m_weaponAnimator = GetComponent<Animator>();
-        m_normalRigit = _rigitPower;
-        m_normalEleke = _elekePower;
-    }
-
-    private void OnEnable()
-    {
-        MainMenu.Instance.DisideElement += this.WeaponMode;
-    }
-
-    private void OnDisable()
-    {
-        //MainMenu.Instance.DisideElement -= this.WeaponMode;
+        _weaponAnimator.SetBool("IsOpen",_isOpen);
     }
 }
