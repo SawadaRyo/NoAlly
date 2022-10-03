9670af44f8f5a0d9b0f3ef525a688475bd35274f
SR21C <rysawad@gmail.com>
2022-10-03 23:34:45 +0900

    [add]WeaponData and ManeuvarData

---
+++ NoAlly/Assets/Excel/Maneuvar/ManeuverDateEntity.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class ManeuverDateEntity
+{
+    public WeaponName Name;
+    public int TargetId;
+    public string ManeuverName;
+    public int Level;
+    public float AttackMagnification;
+}
---
+++ NoAlly/Assets/Excel/Maneuvar/ManeuverTable.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExcelAsset]
+public class ManeuverTable : ScriptableObject
+{
+	public List<ManeuverDateEntity> Maneuvers;
+}
