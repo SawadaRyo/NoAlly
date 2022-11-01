43a6a5ca58cffa3452b9ec606b23513012cbf5fa
SR21C <rysawad@gmail.com>
2022-11-01 22:10:10 +0900

    [add]Sword and LanceAction

---
+++ NoAlly/Assets/Scripts/Weapon/LanceWeapon.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LanceWeapon : CombatWeapon, IWeapon
+{
+    float _normalRigit = 0f;
+    float _normalFrozen = 0f;
+    float _powerUpRigit = 3.5f;
+    float _powerUpFrozen = 5f;
+
+    public override void Awake()
+    {
+        base.Awake();
+        _normalRigit = _rigitPower;
+        _normalFrozen = _frozenPower;
+        _normalHarfExtents = new Vector3(0.1f, 0.8f, 1f);
+        _pawerUpHarfExtents = new Vector3(0.45f, 1.1f, 1f);
+        _harfExtents = _normalHarfExtents;
+    }
+    public override void WeaponMode(ElementType type)
+    {
+        base.WeaponMode(type);
+
+        switch (type)
+        {
+            case ElementType.FROZEN:
+                _isDeformated = true;
+                _harfExtents = _pawerUpHarfExtents;
+                _rigitPower = _powerUpRigit;
+                _frozenPower = _powerUpFrozen;
+                _weaponAnimator.SetBool("IsOpen", true);
+                break;
+            default:
+                _isDeformated = false;
+                _harfExtents = _normalHarfExtents;
+                _rigitPower = _normalRigit;
+                _frozenPower = _normalFrozen;
+                foreach (Renderer bR in _bladeRenderer)
+                {
+                    BladeFadeIn(bR);
+                }
+                break;
+        }
+    }
+}
---
+++ NoAlly/Assets/Scripts/Weapon/SwordWeapon.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordWeapon : CombatWeapon
+{
+    float _normalRigit = 0f;
+    float _normalFire = 0f;
+    float _powerUpRigit = 3.5f;
+    float _powerUpFire = 5f;
+
+    public override void Awake()
+    {
+        base.Awake();
+        _normalRigit = _rigitPower;
+        _normalFire = _elekePower;
+        _normalHarfExtents = new Vector3(0.11f, 1f, 0.1f);
+        _pawerUpHarfExtents = new Vector3(0.12f, 1.1f, 0.1f);
+        _harfExtents = _normalHarfExtents;
+    }
+    public override void WeaponMode(ElementType type)
+    {
+        base.WeaponMode(type);
+
+        switch (type)
+        {
+            case ElementType.FIRE:
+                _isDeformated = true;
+                _harfExtents = _pawerUpHarfExtents;
+                _rigitPower = _powerUpRigit;
+                _firePower = _powerUpFire;
+                _weaponAnimator.SetBool("IsOpen", true);
+                break;
+            default:
+                _isDeformated = false;
+                _harfExtents = _normalHarfExtents;
+                _rigitPower = _normalRigit;
+                _firePower = _normalFire;
+                foreach (Renderer bR in _bladeRenderer)
+                {
+                    BladeFadeIn(bR);
+                }
+                break;
+        }
+    }
+}
