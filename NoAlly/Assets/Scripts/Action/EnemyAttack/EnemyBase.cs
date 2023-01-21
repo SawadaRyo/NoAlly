6a878026afe353b181692cee803cc3a236ee3601
SR21C <rysawad@gmail.com>
2023-01-21 17:30:24 +0900

    [update]stateMachine

--- NoAlly/Assets/Scripts/Action/EnemyAttack/EnemyBase.cs
+++ NoAlly/Assets/Scripts/Action/EnemyAttack/EnemyBase.cs
@@ -1,27 +1,37 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
+using State = StateMachine<EnemyBase>.State;
+
 public abstract class EnemyBase : MonoBehaviour, IObjectPool
 {
-    [SerializeField] protected float _radius = 5f;
-    [SerializeField] protected Animator _enemyAnimator;
-    [SerializeField] protected LayerMask _playerLayer = ~0;
-    [SerializeField] protected Transform _center = default;
-    [SerializeField] ObjectVisual _thisObject = default;
+    [SerializeField, Header("索敵範囲")]
+    protected float _radius = 5f;
+    [SerializeField, Header("このオブジェクトのアニメーター")]
+    protected Animator _enemyAnimator;
+    [SerializeField, Header("索敵用のレイヤー")]
+    protected LayerMask _playerLayer = ~0;
+    [SerializeField, Header("索敵範囲の中心")]
+    protected Transform _center = default;
+    [SerializeField, Header("このオブジェクトのObjectVisual")]
+    ObjectVisual _thisObject = default;
 
+    [Tooltip("このオブジェクトの生死判定")]
     bool _isActive = true;
-    Collider _collider = default;
-    RaycastHit _hitInfo;
+    [Tooltip("ステートマシン")]
+    StateMachine<EnemyBase> _stateMachine = null;
 
-    
+    /// <summary>
+    /// このオブジェクトの生死判定のプロパティ(読み取り専用)
+    /// </summary>
+    public bool IsActive => _isActive;
+    public float Radius => _radius;
+    public Transform Center => _center;
+    public Animator EnemyAnimator => _enemyAnimator;
+    protected int EnemyCurrentState<TState>(TState state) where int => 
 
-
-    public bool IsActive { get => _isActive; set => _isActive = value; }
     public abstract void EnemyAttack();
     public virtual void Start()
     {
-        _collider = GetComponent<Collider>();
+
     }
     public void FixedUpdate()
     {
@@ -33,7 +43,7 @@ public abstract class EnemyBase : MonoBehaviour, IObjectPool
     public virtual void OnTriggerEnter(Collider other) { }
     public virtual void OnTriggerExit(Collider other) { }
 
-    public PlayerContoller InSight()
+    protected PlayerContoller InSight()
     {
         //_center = transform.position;
         Collider[] inSight = Physics.OverlapSphere(_center.position, _radius, _playerLayer);
@@ -48,13 +58,18 @@ public abstract class EnemyBase : MonoBehaviour, IObjectPool
 
     }
 
-    //生成と破壊時の処理
+    /// <summary>
+    /// オブジェクト有効時に呼ぶ関数
+    /// </summary>
     public virtual void Create()
     {
         _isActive = true;
         _thisObject.ActiveWeapon(_isActive);
         _enemyAnimator.SetBool("Death", !_isActive);
     }
+    /// <summary>
+    /// オブジェクト非有効時に呼ぶ関数
+    /// </summary>
     public virtual void Disactive()
     {
         _isActive = false;
@@ -62,17 +77,41 @@ public abstract class EnemyBase : MonoBehaviour, IObjectPool
         _enemyAnimator.SetBool("Death", !_isActive);
 
     }
+    /// <summary>
+    /// オブジェクト生成時に呼ぶ関数
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="Owner"></param>
+    public virtual void DisactiveForInstantiate<T>(T Owner) where T : IObjectGenerator
+    {
+        _isActive = false;
+        _thisObject.ActiveWeapon(_isActive);
+        _stateMachine = new StateMachine<EnemyBase>(this);
+        //HPが0になったとき死亡
+        _stateMachine.AddAnyTransition<Death>((int)EnemyState.Death);
 
+    }
     public virtual void OnDrawGizmos()
     {
         Gizmos.color = Color.green;
         Gizmos.DrawWireSphere(_center.position, _radius);
     }
-    
-
-    public void DisactiveForInstantiate<T>(T Owner) where T : IObjectGenerator
+    protected enum EnemyState : int
     {
-        _isActive = false;
-        _thisObject.ActiveWeapon(_isActive);
+        None,
+        Saerching,
+        Attack,
+        Death
     }
 }
+
+class Death : State
+{
+    protected override void OnEnter(State prevState)
+    {
+        base.OnEnter(prevState);
+        Owner.Disactive();
+    }
+}
+
+
--- NoAlly/Assets/Scripts/Action/StateMachine.cs
+++ NoAlly/Assets/Scripts/Action/StateMachine.cs
@@ -13,233 +13,233 @@ using UniRx.Triggers;
 //2:ステート時の処理の内容
 //3:ステート遷移開始、遷移中、遷移完了それぞれの処理
 //4:自身から遷移できるステートの保持
-public class StateMachine<TOwner>
-{
-    public abstract class State
-    {
-        StateMachine<TOwner> _stateMachine = null;
-        Dictionary<int, State> _nextTransitions = new Dictionary<int, State>();
+//public class StateMachine<TOwner>
+//{
+//    public abstract class State
+//    {
+//        StateMachine<TOwner> _stateMachine = null;
+//        Dictionary<int, State> _nextTransitions = new Dictionary<int, State>();
 
-        protected StateMachine<TOwner> StateMachine => _stateMachine ?? (_stateMachine = new StateMachine<TOwner>());
-        public TOwner Owner => _stateMachine.Owner;
+//        protected StateMachine<TOwner> StateMachine => _stateMachine ?? (_stateMachine = new StateMachine<TOwner>());
+//        public TOwner Owner => _stateMachine.Owner;
 
-        public virtual void StateStart(State state)
-        {
-            OnStateStart(state);
-        }
-        public virtual void StateUpdate(State state) 
-        {
-            OnStateUpdate(state);
-        }
-        public virtual void StateExit(State state) 
-        {
-            OnStateExit(state);
-        }
+//        public virtual void StateStart(State state)
+//        {
+//            OnStateStart(state);
+//        }
+//        public virtual void StateUpdate(State state) 
+//        {
+//            OnStateUpdate(state);
+//        }
+//        public virtual void StateExit(State state) 
+//        {
+//            OnStateExit(state);
+//        }
 
-        protected virtual void OnStateStart(State state) { }
-        protected virtual void OnStateUpdate(State state) { }
-        protected virtual void OnStateExit(State state) { }
-    }
-    public LinkedList<State> states = new LinkedList<State>();
+//        protected virtual void OnStateStart(State state) { }
+//        protected virtual void OnStateUpdate(State state) { }
+//        protected virtual void OnStateExit(State state) { }
+//    }
+//    public LinkedList<State> states = new LinkedList<State>();
 
-    public TOwner Owner { get; }
-}
+//    public TOwner Owner { get; }
+//}
 
 /// <summary>
 /// ステートマシン
 /// </summary>
-//public class StateMachine<TOwner>
-//{
-//    /// <summary>
-//    /// ステートを表すクラス
-//    /// </summary>
-//    public abstract class State
-//    {
-//        /// <summary>
-//        /// このステートを管理しているステートマシン
-//        /// </summary>
-//        protected StateMachine<TOwner> StateMachine => stateMachine;
-//        internal StateMachine<TOwner> stateMachine;
-//        /// <summary>
-//        /// 遷移の一覧
-//        /// </summary>
-//        internal Dictionary<int, State> transitions = new Dictionary<int, State>();
-//        /// <summary>
-//        /// このステートのオーナー
-//        /// </summary>
-//        protected TOwner Owner => stateMachine.Owner;
+public class StateMachine<TOwner>
+{
+    /// <summary>
+    /// ステートを表すクラス
+    /// </summary>
+    public abstract class State
+    {
+        /// <summary>
+        /// このステートを管理しているステートマシン
+        /// </summary>
+        protected StateMachine<TOwner> StateMachine => stateMachine;
+        internal StateMachine<TOwner> stateMachine;
+        /// <summary>
+        /// 遷移の一覧
+        /// </summary>
+        internal Dictionary<int, State> transitions = new Dictionary<int, State>();
+        /// <summary>
+        /// このステートのオーナー
+        /// </summary>
+        protected TOwner Owner => stateMachine.Owner;
 
-//        /// <summary>
-//        /// ステート開始
-//        /// </summary>
-//        internal void Enter(State prevState)
-//        {
-//            OnEnter(prevState);
-//        }
-//        /// <summary>
-//        /// ステートを開始した時に呼ばれる
-//        /// </summary>
-//        protected virtual void OnEnter(State prevState) { }
+        /// <summary>
+        /// ステート開始
+        /// </summary>
+        internal void Enter(State prevState)
+        {
+            OnEnter(prevState);
+        }
+        /// <summary>
+        /// ステートを開始した時に呼ばれる
+        /// </summary>
+        protected virtual void OnEnter(State prevState) { }
 
-//        /// <summary>
-//        /// ステート更新
-//        /// </summary>
-//        internal void Update()
-//        {
-//            OnUpdate();
-//        }
-//        /// <summary>
-//        /// 毎フレーム呼ばれる
-//        /// </summary>
-//        protected virtual void OnUpdate() { }
+        /// <summary>
+        /// ステート更新
+        /// </summary>
+        internal void Update()
+        {
+            OnUpdate();
+        }
+        /// <summary>
+        /// 毎フレーム呼ばれる
+        /// </summary>
+        protected virtual void OnUpdate() { }
 
-//        /// <summary>
-//        /// ステート終了
-//        /// </summary>
-//        internal void Exit(State nextState)
-//        {
-//            OnExit(nextState);
-//        }
-//        /// <summary>
-//        /// ステートを終了した時に呼ばれる
-//        /// </summary>
-//        protected virtual void OnExit(State nextState) { }
-//    }
+        /// <summary>
+        /// ステート終了
+        /// </summary>
+        internal void Exit(State nextState)
+        {
+            OnExit(nextState);
+        }
+        /// <summary>
+        /// ステートを終了した時に呼ばれる
+        /// </summary>
+        protected virtual void OnExit(State nextState) { }
+    }
 
-//    /// <summary>
-//    /// どのステートからでも特定のステートへ遷移できるようにするための仮想ステート
-//    /// </summary>
-//    public sealed class AnyState : State { }
+    /// <summary>
+    /// どのステートからでも特定のステートへ遷移できるようにするための仮想ステート
+    /// </summary>
+    public sealed class AnyState : State { }
 
-//    /// <summary>
-//    /// このステートマシンのオーナー
-//    /// </summary>
-//    public TOwner Owner { get; }
-//    /// <summary>
-//    /// 現在のステート
-//    /// </summary>
-//    public State CurrentState { get; private set; }
+    /// <summary>
+    /// このステートマシンのオーナー
+    /// </summary>
+    public TOwner Owner { get; }
+    /// <summary>
+    /// 現在のステート
+    /// </summary>
+    public State CurrentState { get; private set; }
 
-//    // ステートリスト
-//    private LinkedList<State> states = new LinkedList<State>();
+    // ステートリスト
+    private LinkedList<State> states = new LinkedList<State>();
 
-//    /// <summary>
-//    /// ステートマシンを初期化する
-//    /// </summary>
-//    /// <param name="owner">ステートマシンのオーナー</param>
-//    public StateMachine(TOwner owner)
-//    {
-//        Owner = owner;
-//    }
+    /// <summary>
+    /// ステートマシンを初期化する
+    /// </summary>
+    /// <param name="owner">ステートマシンのオーナー</param>
+    public StateMachine(TOwner owner)
+    {
+        Owner = owner;
+    }
 
-//    /// <summary>
-//    /// ステートを追加する（ジェネリック版）
-//    /// </summary>
-//    public T Add<T>() where T : State, new()
-//    {
-//        var state = new T();
-//        state.stateMachine = this;
-//        states.AddLast(state);
-//        return state;
-//    }
+    /// <summary>
+    /// ステートを追加する（ジェネリック版）
+    /// </summary>
+    public T Add<T>() where T : State, new()
+    {
+        var state = new T();
+        state.stateMachine = this;
+        states.AddLast(state);
+        return state;
+    }
 
-//    /// <summary>
-//    /// 特定のステートを取得、なければ生成する
-//    /// </summary>
-//    public T GetOrAddState<T>() where T : State, new()
-//    {
-//        foreach (var state in states)
-//        {
-//            if (state is T result)
-//            {
-//                return result;
-//            }
-//        }
-//        return Add<T>();
-//    }
+    /// <summary>
+    /// 特定のステートを取得、なければ生成する
+    /// </summary>
+    public T GetOrAddState<T>() where T : State, new()
+    {
+        foreach (var state in states)
+        {
+            if (state is T result)
+            {
+                return result;
+            }
+        }
+        return Add<T>();
+    }
 
-//    /// <summary>
-//    /// 遷移を定義する
-//    /// </summary>
-//    /// <param name="eventId">イベントID</param>
-//    public void AddTransition<TFrom, TTo>(int eventId)
-//        where TFrom : State, new()
-//        where TTo : State, new()
-//    {
-//        var from = GetOrAddState<TFrom>();
-//        if (from.transitions.ContainsKey(eventId))
-//        {
-//            // 同じイベントIDの遷移を定義済
-//            throw new System.ArgumentException(
-//                $"ステート'{nameof(TFrom)}'に対してイベントID'{eventId.ToString()}'の遷移は定義済です");
-//        }
+    /// <summary>
+    /// 遷移を定義する
+    /// </summary>
+    /// <param name="eventId">イベントID</param>
+    public void AddTransition<TFrom, TTo>(int eventId)
+        where TFrom : State, new()
+        where TTo : State, new()
+    {
+        var from = GetOrAddState<TFrom>();
+        if (from.transitions.ContainsKey(eventId))
+        {
+            // 同じイベントIDの遷移を定義済
+            throw new System.ArgumentException(
+                $"ステート'{nameof(TFrom)}'に対してイベントID'{eventId.ToString()}'の遷移は定義済です");
+        }
 
-//        var to = GetOrAddState<TTo>();
-//        from.transitions.Add(eventId, to);
-//    }
+        var to = GetOrAddState<TTo>();
+        from.transitions.Add(eventId, to);
+    }
 
-//    /// <summary>
-//    /// どのステートからでも特定のステートへ遷移できるイベントを追加する
-//    /// </summary>
-//    /// <param name="eventId">イベントID</param>
-//    public void AddAnyTransition<TTo>(int eventId) where TTo : State, new()
-//    {
-//        AddTransition<AnyState, TTo>(eventId);
-//    }
+    /// <summary>
+    /// どのステートからでも特定のステートへ遷移できるイベントを追加する
+    /// </summary>
+    /// <param name="eventId">イベントID</param>
+    public void AddAnyTransition<TTo>(int eventId) where TTo : State, new()
+    {
+        AddTransition<AnyState, TTo>(eventId);
+    }
 
-//    /// <summary>
-//    /// ステートマシンの実行を開始する（ジェネリック版）
-//    /// </summary>
-//    public void Start<TFirst>() where TFirst : State, new()
-//    {
-//        Start(GetOrAddState<TFirst>());
-//    }
+    /// <summary>
+    /// ステートマシンの実行を開始する（ジェネリック版）
+    /// </summary>
+    public void Start<TFirst>() where TFirst : State, new()
+    {
+        Start(GetOrAddState<TFirst>());
+    }
 
-//    /// <summary>
-//    /// ステートマシンの実行を開始する
-//    /// </summary>
-//    /// <param name="firstState">起動時のステート</param>
-//    /// <param name="param">パラメータ</param>
-//    public void Start(State firstState)
-//    {
-//        CurrentState = firstState;
-//        CurrentState.Enter(null);
-//    }
+    /// <summary>
+    /// ステートマシンの実行を開始する
+    /// </summary>
+    /// <param name="firstState">起動時のステート</param>
+    /// <param name="param">パラメータ</param>
+    public void Start(State firstState)
+    {
+        CurrentState = firstState;
+        CurrentState.Enter(null);
+    }
 
-//    /// <summary>
-//    /// ステートを更新する
-//    /// </summary>
-//    public void Update()
-//    {
-//        CurrentState.Update();
-//    }
+    /// <summary>
+    /// ステートを更新する
+    /// </summary>
+    public void Update()
+    {
+        CurrentState.Update();
+    }
 
-//    /// <summary>
-//    /// イベントを発行する
-//    /// </summary>
-//    /// <param name="eventId">イベントID</param>
-//    public void Dispatch(int eventId)
-//    {
-//        State to;
-//        if (!CurrentState.transitions.TryGetValue(eventId, out to))
-//        {
-//            if (!GetOrAddState<AnyState>().transitions.TryGetValue(eventId, out to))
-//            {
-//                // イベントに対応する遷移が見つからなかった
-//                return;
-//            }
-//        }
-//        Change(to);
-//    }
+    /// <summary>
+    /// イベントを発行する
+    /// </summary>
+    /// <param name="eventId">イベントID</param>
+    public void Dispatch(int eventId)
+    {
+        State to;
+        if (!CurrentState.transitions.TryGetValue(eventId, out to))
+        {
+            if (!GetOrAddState<AnyState>().transitions.TryGetValue(eventId, out to))
+            {
+                // イベントに対応する遷移が見つからなかった
+                return;
+            }
+        }
+        Change(to);
+    }
 
-//    /// <summary>
-//    /// ステートを変更する
-//    /// </summary>
-//    /// <param name="nextState">遷移先のステート</param>
-//    private void Change(State nextState)
-//    {
-//        CurrentState.Exit(nextState);
-//        nextState.Enter(CurrentState);
-//        CurrentState = nextState;
-//    }
-//}
+    /// <summary>
+    /// ステートを変更する
+    /// </summary>
+    /// <param name="nextState">遷移先のステート</param>
+    private void Change(State nextState)
+    {
+        CurrentState.Exit(nextState);
+        nextState.Enter(CurrentState);
+        CurrentState = nextState;
+    }
+}
