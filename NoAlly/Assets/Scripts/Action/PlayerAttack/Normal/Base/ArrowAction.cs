87de5fbe1353f45bfa05551ae9716e415c5e8a20
SR21C <rysawad@gmail.com>
2023-03-29 23:33:21 +0900

    [add]ArrowAction

---
+++ NoAlly/Assets/Scripts/Action/PlayerAttack/Normal/Base/ArrowAction.cs
@@ -0,0 +1,22 @@
+using System;
+using UnityEngine;
+
+public class ArrowAction : WeaponAction,IArrowAction
+{
+    public BulletType WeaponChargeAttackMethod(float chrageCount, float[] chargeLevels, ElementType elementType)
+    {
+        //ã≠âªíe
+        if (chrageCount > chargeLevels[0] && chrageCount <= chargeLevels[1])
+        {
+            return BulletType.STRENGTHEN;
+        }
+        //ëÂñC
+        else if (chrageCount > chargeLevels[1])
+        {
+            return BulletType.CANNON;
+        }
+
+        //í èÌíe
+        return BulletType.NORMAL;
+    }
+}
