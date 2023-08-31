82522c1f95d895dac0c8310c0b31ca655531ec10
SR21C <rysawad@gmail.com>
2023-08-31 23:16:27 +0900

    [fix]フォルダ移動

---
+++ NoAlly/Assets/Enemy/Scripts/StateOfEnemy.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum StateOfEnemy : int
+{
+    None, //
+    Move, //ˆÚ“®
+    Saerching, //ƒvƒŒƒCƒ„[‚ğ‘{õ’†
+    BattlePosture, //í“¬‘Ô¨
+    Attack, //UŒ‚
+    Death //€–S
+}
